8e07e960c26fd4a47ab2badc8f175e24247ee1cc
edw <edwin@baculsoft.com>
2015-08-04 23:59:56 +0700

    initial commit

---
+++ readme.md
@@ -0,0 +1,39 @@
+A Simple Test Case for Simulating a Race Condition 
+===================
+
+Tools
+-------------------
+* Programming Language - Java
+* IDE - Netbeans 8.0.2
+* DB - Oracle
+* Build Tool - Maven
+
+Output Example
+--------------------
+>Thread2 query nomer surat S-302PKP/WPJ.10/KP.1003/2015 for 6 ms 
+
+>Thread1 query nomer surat S-302PKP/WPJ.10/KP.1003/2015 for 6 ms 
+
+>Thread2 query nomer surat S-303PKP/WPJ.10/KP.1003/2015 for 4 ms 
+
+>Thread1 query nomer surat S-303PKP/WPJ.10/KP.1003/2015 for 4 ms  -- > **Duplicate**
+
+>Thread1 query nomer surat S-304PKP/WPJ.10/KP.1003/2015 for 4 ms 
+
+>Thread2 query nomer surat S-304PKP/WPJ.10/KP.1003/2015 for 4 ms  -- > **Duplicate**
+
+>Thread1 query nomer surat S-305PKP/WPJ.10/KP.1003/2015 for 4 ms 
+
+>Thread2 query nomer surat S-305PKP/WPJ.10/KP.1003/2015 for 5 ms  -- > **Duplicate**
+
+
+Query
+--------------------
+``` sql
+SELECT COUNT(1) AS total,
+  X_DEBUG.value
+FROM X_DEBUG
+GROUP BY X_DEBUG.value
+HAVING COUNT(1) > 1
+ORDER BY total;
+```
---
+++ src/main/java/id/go/pajak/raceconditionexample/Main.java
@@ -0,0 +1,100 @@
+package id.go.pajak.raceconditionexample;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Types;
+import java.util.Stack;
+import oracle.jdbc.OracleDriver;
+
+/**
+ * 
+ *  untuk simulasi race condition dalam pengambilan nomer SKT menggunakan package oracle
+ * 
+ */
+public class Main {
+
+    /**
+     *  query untuk call oracle package
+     */
+    private static String S = "CALL PKG_SEQ.GET_NO_DOK(? , ? , ?)";
+
+    private static Connection getConnection() throws Throwable {
+        Class.forName(OracleDriver.class.getName());
+        return DriverManager.getConnection("jdbc:oracle:thin:piloting/password@//localhost:1521/XE");
+    }
+
+    /**
+     * inner class untuk thread
+     */
+    private static class ThreadNoDoc extends Thread {
+
+        private final String type;
+        private final String kdKpp;
+        private final Stack<String> stack;
+
+        public ThreadNoDoc(String name, String type, String kdKpp, Stack stack) {
+            super(name);
+            this.type = type;
+            this.kdKpp = kdKpp;
+            this.stack = stack;
+        }
+
+        @Override
+        public void run() {
+            Connection con = null;
+            CallableStatement callable = null;
+            try {
+                con = getConnection();
+                while (true) {
+                    long tm = System.currentTimeMillis();
+                    callable = con.prepareCall(S);
+                    callable.setMaxRows(1);
+                    callable.setFetchSize(1);
+                    callable.setString(1, type);
+                    callable.setString(2, kdKpp);
+                    callable.registerOutParameter(3, Types.VARCHAR);
+                    callable.executeUpdate();
+                    String trxNo = callable.getString(3);
+                    tm = System.currentTimeMillis() - tm;
+                    if (stack.contains(trxNo)) {
+                        System.out.println(getName()+ " query nomer surat " + trxNo + " for "+ tm + " ms " + " -- > Duplicate");
+                    } else {
+                        System.out.println(getName()+ " query nomer surat " + trxNo + " for "+ tm + " ms ");
+                        stack.add(trxNo);
+                    }
+                    try {
+                        callable.close();
+                    } catch (Throwable t) {
+                        t.printStackTrace();
+                    }
+                    Thread.sleep(1000);
+                }
+            } catch (Throwable t) {
+                t.printStackTrace();
+            } finally {
+                try {
+                    con.close();
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+
+                try {
+                    callable.close();
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Stack<String> stack = new Stack<>();
+        ThreadNoDoc t1 = new ThreadNoDoc("Thread1", "RG02", "511", stack);
+        ThreadNoDoc t2 = new ThreadNoDoc("Thread2", "RG02", "511", stack);
+
+        t1.start();
+        Thread.sleep(50);
+        t2.start();
+    }
+}
